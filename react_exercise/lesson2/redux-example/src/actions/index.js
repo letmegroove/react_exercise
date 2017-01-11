dca2c21a8ba47e0d2519f38265819629f6775306
Travis <letmegroove@gmail.com>
2017-01-11 23:05:20 +0900

    first commit

---
+++ react_exercise/lesson2/redux-example/src/actions/index.js
@@ -0,0 +1,22 @@
+import * as types from './ActionTypes';
+
+//Action 생성자 함수 작성
+
+export function increment(){
+	return {
+		type: types.INCREMENT
+	};
+}
+
+export function decrement(){
+	return{
+		type: types.DECREMENT
+	};
+}
+
+export function setColor(color){
+	return{
+		type: types.SET_COLOR,
+		color  //color: color 와 동일
+	};
+}
